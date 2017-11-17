a608ba0a000a593168b903bd73ec598bfd0cbfa8
liyang <1911178174@qq.com>
2017-11-17 09:24:12 +0800

    修改启动类

--- ldf-war-demo/src/main/java/org/loushang/ldf/Application.java
+++ ldf-war-demo/src/main/java/org/loushang/ldf/Application.java
@@ -1,14 +1,10 @@
 package org.loushang.ldf;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
