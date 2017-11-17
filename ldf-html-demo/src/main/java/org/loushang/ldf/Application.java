a6049f234ea7e0803937a335e60e09575ea70280
liyang <1911178174@qq.com>
2017-11-17 12:51:27 +0800

    添加angularjs案例

---
+++ ldf-html-demo/src/main/java/org/loushang/ldf/Application.java
@@ -0,0 +1,16 @@
+package org.loushang.ldf;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SpringBootApplication
+@MapperScan(basePackages = "org.loushang.ldf.dao")
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
